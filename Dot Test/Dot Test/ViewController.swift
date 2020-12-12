51e04c1df4fb7a12ab73dd0d7598213f57ce2c8f
Samsul Arifin <arfn.samsul@gmail.com>
2020-12-12 19:17:17 +0700

    initial commit

---
+++ Dot Test/Dot Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dot Test
+//
+//  Created by Samsul Arifin on 12/12/20.
+//  Copyright © 2020 Samsul Arifin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Dot Test/Pods/Target Support Files/Pods-Dot Test-Dot TestUITests/Pods-Dot Test-Dot TestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dot_Test_Dot_TestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Dot_Test_Dot_TestUITests
+@end
